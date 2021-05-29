c8d5eb3e06820e9e2d219d6a53ad490608589261
Albert Araque <aaraquedesarrollo@gmail.com>
2021-05-29 12:29:17 +0200

    creadas funciones sin codigo en index.js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const equiposMayoresEdad = (arrayEquipos) => {};
+
+const equiposProvincia = (arrayEquipos) => {};
+
+const provincias = (arrayEquipos) => {};
+
+const puestos = (arrayEquipos) => {};
+
+const edadMedia = (arrayEquipos) => {};
+
+const equiposPorEdad = (arrayEquipos) => {};
+
+const equiposTipo = (arrayEquipos) => {};
+
+const trabajadoresTipo = (arrayEquipos) => {};
+
+const equiposPorTipo = (arrayEquipos) => {};
+
+const equiposTipoLocalidad = (arrayEquipos) => {};
+
+const resumenEquipos = (arrayEquipos) => {};
