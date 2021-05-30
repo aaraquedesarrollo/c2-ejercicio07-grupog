de75c828808f4ddd9b27fa9a2bc04f9d06bb283f
federicotiralongo95@gmail.com <federicotiralongo95@gmail.com>
2021-05-30 22:13:28 +0200

    funciones 4,5,6 acabada

--- js/index.js
+++ js/index.js
@@ -1,23 +1,80 @@
+const esIgual = (texto1, texto2) =>
+  texto1.toLowerCase() === texto2.toLowerCase();
+
 const equiposMayoresEdad = (arrayEquipos, edad) =>
   arrayEquipos.filter((equipo) => equipo.asignado.empleado.edad >= edad);
 
 const equiposProvincia = (arrayEquipos, provincia) =>
-  arrayEquipos.filter((equipo) => equipo.asignado.provincia === provincia);
+  arrayEquipos.filter((equipo) =>
+    esIgual(equipo.asignado.provincia, provincia)
+  );
 
-const provincias = (arrayEquipos) => {};
+const provincias = (arrayEquipos) => [
+  ...new Set(arrayEquipos.map((equipo) => equipo.asignado.provincia)),
+];
 
-const puestos = (arrayEquipos) => {};
+const puestos = (arrayEquipos) =>
+  arrayEquipos.map((equipo) => equipo.asignado.empleado.puesto);
 
-const edadMedia = (arrayEquipos) => {};
+function edadMedia(array) {
+  const edadMediaEmpleados = array
+    .map((equipos) => equipos.asignado.empleado.edad)
+    .reduce(
+      (contador, edades, i, array) => edades / array.length + contador,
+      0
+    );
+  return `${edadMediaEmpleados} años de media`;
+}
+const equiposPorEdad = (arrayEquipos) =>
+arrayEquipos.sort((a, b) => {
+  const edadA = a.asignado.empleado.edad;
+  const edadB = b.asignado.empleado.edad;
+  if (edadA > edadB) {
+    return 1;
+  }
+  if (edadA < edadB) {
+    return -1;
+  }
+  return 0;
+});
 
-const equiposPorEdad = (arrayEquipos) => {};
+const equiposTipo = (arrayEquipos, tipo) =>
+  arrayEquipos.filter((equipo) => esIgual(equipo.tipo, tipo));
 
-const equiposTipo = (arrayEquipos, tipo) => {};
+const trabajadoresTipo = (arrayEquipos, tipo) =>
+  arrayEquipos
+    .filter((equipo) => esIgual(equipo.tipo, tipo))
+    .map((equipo) => equipo.asignado.empleado);
 
-const trabajadoresTipo = (arrayEquipos, tipo) => {};
+const equiposPorTipo = (arrayEquipos) => [
+  ...new Set(
+    ...arrayEquipos.map((equipo) => [
+      {
+        tipo: "Portátil",
+        equipos: arrayEquipos.filter((equipo) =>
+          esIgual(equipo.tipo, "portátil")
+        ),
+      },
+      {
+        tipo: "Sobremesa",
+        equipos: arrayEquipos.filter((equipo) =>
+          esIgual(equipo.tipo, "sobremesa")
+        ),
+      },
+    ])
+  ),
+];
 
-const equiposPorTipo = (arrayEquipos) => {};
+const equiposTipoLocalidad = (arrayEquipos, tipo, localidad) =>
+  arrayEquipos.filter(
+    (equipo) =>
+      esIgual(equipo.tipo, tipo) &&
+      esIgual(equipo.asignado.poblacion, localidad)
+  );
 
-const equiposTipoLocalidad = (arrayEquipos, tipo, localidad) => {};
-
-const resumenEquipos = (arrayEquipos) => {};
+const resumenEquipos = (arrayEquipos) =>
+  arrayEquipos.map((equipo) => [
+    (id = equipo.id),
+    (poblacion = equipo.asignado.poblacion),
+    (provincia = equipo.asignado.provincia),
+  ]);
